fa687d72fa3cb1d2e0b1da2a198e0446ad266f8f
Diogo Tallys <dtma@ic.ufal.br>
2026-02-09 17:35:32 -0300

    delete all

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-import { Elysia } from "elysia";
-import { drizzle } from 'drizzle-orm/node-postgres';
-
-const db = drizzle(process.env.DATABASE_URL!);
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
-
